94529a0f0e6ab4ff6bf8f680655a6ead3c680a26
kajal-sharma-20 <kajal0120sharma@gmail.com>
2025-07-30 23:02:29 +0530

    Update todomodel.js

--- backend/model/todomodel.js
+++ backend/model/todomodel.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 const todoSchema = new mongoose.Schema({
   title: { type: String, required: true },
   desc: { type: String, required: true }
-});
+},{ timestamps: true });
 
 const Todo = mongoose.model("todo", todoSchema);
 export default Todo;
